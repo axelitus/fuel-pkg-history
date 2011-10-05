axelitus/fuel-pkg-history

commit f3dcb4d0d447bb99b76b4deb0a9770ddbe607a76

Added condition to check for the extension to collect

classes/history/driver/gc/file.php
@@ -35,7 +35,7 @@ class History_Driver_GC_File extends History_Driver_GC
 			while (($file = readdir($handle)) !== false)
 			{
 				$fullpath = $path . DS . $file;
-				if(filetype($fullpath) == 'file' && strpos($file, $prefix) === 0 && filemtime($fullpath) < $expire)
+				if(filetype($fullpath) == 'file' && strpos($file, $prefix) === 0 && substr($filename, -strlen($ext)) === $ext &&  filemtime($fullpath) < $expire)
 				{
 					if(@unlink($fullpath))
 					{

commit 6d2f4ef2a34e5ca414780b8a779065e8dc714e3b

Fixed a bug introduced by commit f3dcb4d where the File Driver GC would fail to collect because a variable was mispelled Reference: (https://github.com/axelitus/fuel-pkg-history/commit/f3dcb4d0d447bb99b76b4deb0a9770ddbe607a76#classes/history/driver/gc/file.php)

classes/history/driver/gc/file.php
@@ -48,7 +48,7 @@ class History_Driver_GC_File extends History_Driver_GC
 			while (($file = readdir($handle)) !== false)
 			{
 				$fullpath = $path . DS . $file;
-				if(filetype($fullpath) == 'file' && strpos($file, $prefix) === 0 && substr($filename, -strlen($ext)) === $ext &&  filemtime($fullpath) < $expire)
+				if(filetype($fullpath) == 'file' && strpos($file, $prefix) === 0 && substr($file, -strlen($ext)) === $ext &&  filemtime($fullpath) < $expire)
 				{
 					if(@unlink($fullpath))
 					{