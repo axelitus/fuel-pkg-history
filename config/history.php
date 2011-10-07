d17313e90a77637f2af042d4f8c4cf579d9a31c9
Axel Pardemann <axelitus2005@gmail.com>
2011-10-07 16:59:10 -0500

    Change some comments to be more explicit. Set correct defaults

--- config/history.php
+++ config/history.php
@@ -12,61 +12,61 @@
 
 // @formatter:off
 return array(
-	// default ID for history tag in Session (optional, default = 'history')
+	// Default ID for history tag in Session (optional, default = 'history').
 	'history_id' => 'history',
-	// Contains the options for the driver
+	// Contains the options for the driver.
 	'driver' => array(
-		// The name of the driver to be used (optional, default = 'file'). Options: file|database|session
-		'name' => 'file',
-		// The compression options to be used
+		// The name of the driver to be used (optional, default = 'file'). Options: file|database|session.
+		'name' => 'session',
+		// The compression options to be used.
 		'compression' => array(
-			// Whether to compress the data before encoding or not. It uses gzcompress (optional, default = true)
+			// Whether to compress the data before encoding or not (optional, default = true).
 			'active' => true,
-			// The format of the compression to be used (optional, default = 'zlib'). Options: zlib|deflate
+			// The format of the compression to be used (optional, default = 'zlib'). Options: zlib|deflate.
 			'format' => 'zlib',
-			// The level of compression to use (optional, default = 5)
-			'level' => 9
+			// The level of compression to use (optional, default = 5).
+			'level' => 5
 		),
-		// Whether to encode the entries (using Fuel\Crypt class) or not (optional, default = true)
+		// Whether to encode the entries (using Fuel\Crypt class) or not (optional, default = true).
 		'secure' => true,
-		// The length of the hash to be used to identify the stack
+		// The length of the hash to be used to identify the stack. The max length is 40 (optional, default = 8).
 		'hash_length' => 8,
-		// Contains the specific config for the file driver (other drivers will ignore it). This can be ommited when using other drivers
+		// Contains the specific config for the file driver (other drivers will ignore it). This can be ommited when using other drivers.
 		'file' => array(
-			// The path to the file to be used for archiving. (optional, default = sys_get_temp_dir())
+			// The path to the file to be used for storing. (optional, default = '' [uses sys_get_temp_dir()]).
 			'path' => '',
-			// The prefix used for the filenames
+			// The prefix used for the filenames (optional, default = 'his_').
 			'prefix' => 'his_',
-			// The file extension to be used (you must include the dot)
+			// The file extension to be used [you must include the dot] (optional, default = '.tmp').
 			'extension' => '.tmp'
 		),
-		// Contains the specific config for the database driver (other drivers will ignore it). This can be ommited when using other drivers
+		// Contains the specific config for the database driver (other drivers will ignore it). This can be ommited when using other drivers.
 		'database' => array(
-			// The table name to be used for storing the stack
+			// The table name to be used for storing the stack (optional, default = 'history').
 			'table' => 'history',
-			// Determines if tabel should be created if it does not exist
+			// Determines if tabel should be created if it does not exist (optional, default = true).
 			'auto_create' => true
 		),
-		// Contains the specific config for the session driver (other drivers will ignore it). This can be ommited when using other drivers
+		// Contains the specific config for the session driver (other drivers will ignore it). This can be ommited when using other drivers.
 		'session' => array(
 		),
-		// Garbage Collector options for driver
+		// Garbage Collector options for driver.
 		'gc' => array(
-			// Whether to use Garbage Collection or not. You should leave this on to prevent data flooding and even collisions.
+			// Whether to use Garbage Collection or not. You should leave this on to prevent data flooding and even collisions (optional, default = true).
 			'active' => true,
-			// Seconds that will last the unmodified items (files or records depending on driver) before the garbage collector deletes them (optional, default = 900 [15 minutes])
+			// Seconds that will last the unmodified items (files or records depending on driver) before the garbage collector deletes them (optional, default = 900 [15 minutes]).
 			'threshold' => 900,
-			// Probability % (between 0 and 100) for garbage collection
+			// Probability % (between 0 and 100) for garbage collection (optional, default = 5).
 			'probability' => 5
 		)
 	),
-	// Contains the options for the entries
+	// Contains the options for the entries.
 	'entries' => array(
-		// How many entries should we collect? (optional, default = 15, use 0 for unlimited)
+		// How many entries should we collect? (optional, default = 15 [use 0 for unlimited]).
 		'limit' => 15,
-		// Do not allow duplicate entries by refresh (optional, default = true)
+		// Do not allow duplicate entries by refresh (optional, default = true).
 		'prevent_refresh' => true,
-		// Whether to save the full post data in the entry or just the post data hash (optional, default = true)
+		// Whether to save the full post data in the entry or just the post data hash (optional, default = false).
 		'use_full_post' => false
 	)
 );
